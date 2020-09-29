348e9254bfcb4d25d12c57b643d3b93aa9f1c126
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-09-29 21:10:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css2?family=Teko&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/style-mobile.css">
-        <title>Learning Task 2.3</title>
+        <title>Learning Evidence</title>
     </head>
     <body class="grey">
         <!--nav-->
